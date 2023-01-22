0e60d290a3c5ef9dc18927f47f0401a40a014b3e
Cocodilla <evgeniacesar@gmail.com>
2023-01-22 21:23:57 +0300

    Start

--- app/src/main/java/com/example/filewriterreader/MainActivity.java
+++ app/src/main/java/com/example/filewriterreader/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.filewriterreader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String FILE_NAME = " content.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // save files
+    public void saveText(View view) {
+        FileOutputStream fos = null;
+        try {
+            EditText textBox = (EditText) view.findViewById(R.id.editor);
+            String text = textBox.getText().toString();
+
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            Toast.makeText(this, "Файл успешно сохранён", Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+    //open file
 }
