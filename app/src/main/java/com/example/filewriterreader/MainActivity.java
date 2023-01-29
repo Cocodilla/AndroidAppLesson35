8d56eed4bff4b1ad47e5f090aaabf4479348d19d
Cocodilla <evgeniacesar@gmail.com>
2023-01-29 20:53:38 +0300

    Errors

--- app/src/main/java/com/example/filewriterreader/MainActivity.java
+++ app/src/main/java/com/example/filewriterreader/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -49,5 +51,33 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
     //open file
+    public void openText(View view) {
+        FileInputStream fin = null;
+        TextView textView = (TextView) view.findViewById(R.id.text);
+        try {
+            fin = openFileInput(FILE_NAME);
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String(bytes);
+            textView.setText(text);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            try {
+                if (fin != null) {
+                    fin.close();
+                }
+            }catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
